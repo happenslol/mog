80a922785cb63f7c1f17986678d45e098fe97a79
Hilmar Wiegand <me@hwgnd.de>
2021-12-18 03:15:44 +0100

    Switch to loader for struct parsing

--- go.mod
+++ go.mod
@@ -12,15 +12,16 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tidwall/pretty v1.0.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
-	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- main.go
+++ main.go
@@ -4,18 +4,13 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"go/ast"
 	"go/format"
-	"go/printer"
-	"go/token"
 	"io"
 	"os"
-	"reflect"
 	"strings"
 
+	"github.com/happenslol/mog/codegen"
 	"github.com/happenslol/mog/templates"
-	"go.mongodb.org/mongo-driver/bson/bsoncodec"
-	"golang.org/x/tools/go/packages"
 	"gopkg.in/yaml.v3"
 )
 
@@ -37,7 +32,7 @@ type CollectionConfig struct {
 type TemplateInput struct {
 	PackageName string
 	Collections map[string]CollectionInput
-	Structs     []Strct
+	Structs     []codegen.Strct
 	Imports     []string
 }
 
@@ -45,48 +40,10 @@ type CollectionInput struct {
 	ModelType      string
 	CollectionType string
 	IDType         string
+	Imports        []string
 }
 
-type Strct struct {
-	Name   string
-	Fields []Field
-}
-
-type Field struct {
-	Name    string
-	BsonKey string
-	Type    string
-}
-
-var builtinPrimitives = []string{
-	".uint",
-	".uintptr",
-	".uint8",
-	".uint16",
-	".uint32",
-	".uint64",
-
-	".int",
-	".int8",
-	".int16",
-	".int32",
-	".int64",
-
-	".float32",
-	".float64",
-
-	".complex64",
-	".complex128",
-
-	".byte",
-	".rune",
-	".string",
-	".bool",
-
-	"time.Time",
-}
-
-var defaultUsedImports = []string{
+var defaultPrimitives = []string{
 	"time",
 }
 
@@ -105,57 +62,54 @@ func main() {
 		panic(fmt.Sprintf("Failed to parse config: %s", err.Error()))
 	}
 
-	strcts := map[string]Strct{}
-	knownImports := map[string]string{}
-	usedImports := map[string]struct{}{}
 	modelImports := map[string]struct{}{}
-
-	for _, imp := range defaultUsedImports {
-		modelImports[imp] = struct{}{}
-	}
-
-	primitives := map[string]struct{}{}
-	for _, p := range builtinPrimitives {
-		primitives[p] = struct{}{}
-	}
-
-	for _, p := range config.Primitives {
-		primitives[p] = struct{}{}
-	}
 
 	input := &TemplateInput{
 		PackageName: config.Output.Package,
 		Collections: map[string]CollectionInput{},
-		Structs:     []Strct{},
-		Imports:     []string{},
+		Structs:     []codegen.Strct{},
+		Imports:     []string{}}
+
+	// If we just mark all primitives as already
+	// known, they will be skipped when we reach them
+	strcts := map[string]codegen.Strct{}
+	for _, s := range config.Primitives {
+		strcts[s] = codegen.Strct{}
+	}
+
+	for _, s := range defaultPrimitives {
+		strcts[s] = codegen.Strct{}
 	}
 
 	for colName, col := range config.Collections {
-		basePkg, name, err := splitPackageUID(col.Model)
+		pkg, name, err := splitPackageUID(col.Model)
 		if err != nil {
 			panic(err)
 		}
 
-		if err := generateStructMethods(col.Model, basePkg, strcts, primitives,
-			knownImports, usedImports, modelImports); err != nil {
-			panic(err)
-		}
+		codegen.LoadModel(pkg, name, strcts)
 
-		parts := strings.Split(basePkg, "/")
+		parts := strings.Split(pkg, "/")
 		modelType := fmt.Sprintf("%s.%s", parts[len(parts)-1], name)
 
 		// Find our struct from the struct list. It's guaranteed to
 		// be there if the previous function did not error out.
 		strct := strcts[col.Model]
-		idType := findIDType(strct, modelImports)
+		idField := findIDType(strct)
 
 		collectionType := fmt.Sprintf("%sCollection", strings.Title(colName))
 
-		input.Collections[colName] = CollectionInput{
+		result := CollectionInput{
 			ModelType:      modelType,
-			IDType:         idType,
+			IDType:         idField.Type,
 			CollectionType: collectionType,
 		}
+
+		for imp := range idField.Imports {
+			result.Imports = append(result.Imports, imp)
+		}
+
+		input.Collections[colName] = result
 	}
 
 	for _, s := range strcts {
@@ -176,95 +130,22 @@ func main() {
 	}
 }
 
-func findIDType(strct Strct, modelImports map[string]struct{}) string {
+func findIDType(strct codegen.Strct) codegen.Field {
 	for _, fld := range strct.Fields {
 		// If one of the existing fields is the index field, we don't
 		// need any extra imports since it will already be imported
 		// for the field helper methods
 		if fld.BsonKey == "_id" {
-			return fld.Type
+			return fld
 		}
 	}
 
 	// If we drop through, there's no explicit ID key and
 	// we use primitive.ObjectID, which also needs an import.
-	modelImports["go.mongodb.org/mongo-driver/bson/primitive"] = struct{}{}
-	return "primitive.ObjectID"
-}
-
-func generateStructMethods(
-	uid string,
-	basePkg string,
-	strcts map[string]Strct,
-	primitives map[string]struct{},
-	knownImports map[string]string,
-	usedImports map[string]struct{},
-	modelImports map[string]struct{},
-) error {
-	modelImports[basePkg] = struct{}{}
-
-	structQueue := []string{uid}
-	dotImports := []string{}
-
-	for len(structQueue) != 0 {
-		uid := structQueue[0]
-		structQueue = structQueue[1:]
-
-		if _, ok := strcts[uid]; ok {
-			continue
-		}
-
-		if _, ok := primitives[uid]; ok {
-			continue
-		}
-
-		if strings.HasPrefix(uid, ".") {
-			uid = basePkg + uid
-		}
-
-		// Check again if we had a dot import before
-		if _, ok := primitives[uid]; ok {
-			continue
-		}
-
-		spec, fset, foundImports, err := findTypeSpec(uid)
-		if err != nil {
-			return err
-		}
-
-		for _, imp := range foundImports {
-			path := strings.ReplaceAll(imp.Path.Value, "\"", "")
-			parts := strings.Split(path, "/")
-			name := parts[len(parts)-1]
-
-			if imp.Name != nil {
-				if imp.Name.Name == "." {
-					dotImports = append(dotImports, imp.Name.Name)
-					continue
-				}
-
-				name = imp.Name.Name
-			}
-
-			knownImports[name] = path
-		}
-
-		strctType, ok := spec.Type.(*ast.StructType)
-		if !ok {
-			return fmt.Errorf("Type `%s` is not a struct", uid)
-		}
-
-		fields, typs := parseFields(fset, strctType.Fields.List,
-			knownImports, usedImports, primitives)
-
-		strcts[uid] = Strct{
-			Name:   spec.Name.Name,
-			Fields: fields}
-
-		structQueue = append(structQueue, typs...)
-	}
-
-	return nil
+	return codegen.Field{
+		Type: "primitive.ObjectID",
+		Imports: map[string]struct{}{
+			"go.mongodb.org/mongo-driver/bson/primitive": {}}}
 }
 
 func writeFormattedOutput(input *TemplateInput, out io.Writer) error {
@@ -280,182 +161,6 @@ func writeFormattedOutput(input *TemplateInput, out io.Writer) error {
 
 	_, err = out.Write(formatted)
 	return err
-}
-
-func parseFields(
-	fset *token.FileSet,
-	flds []*ast.Field,
-	knownImports map[string]string,
-	usedImports map[string]struct{},
-	primitives map[string]struct{},
-) (
-	fields []Field,
-	referencedTypes []string,
-) {
-	fields = []Field{}
-
-	for _, fld := range flds {
-		spec := fld.Type
-		foundTypes := parseReferencedTypes(spec, knownImports, usedImports, primitives)
-		referencedTypes = append(referencedTypes, foundTypes...)
-
-		field := parseField(fld, fset)
-		if field == nil {
-			continue
-		}
-
-		fields = append(fields, *field)
-	}
-
-	return
-}
-
-func parseField(fld *ast.Field, fset *token.FileSet) *Field {
-	if fld.Names == nil {
-		// TODO: Implement embedded types
-		return nil
-	}
-
-	// TODO: This will not respect import aliases
-	b := bytes.NewBufferString("")
-	if err := printer.Fprint(b, fset, fld.Type); err != nil {
-		panic(fmt.Sprintf("Failed to print type: %s", err.Error()))
-	}
-
-	for _, name := range fld.Names {
-		var tag reflect.StructTag
-		if fld.Tag != nil {
-			unquoted := strings.ReplaceAll(fld.Tag.Value, "`", "")
-			tag = reflect.StructTag(unquoted)
-		}
-
-		dummyStructField := reflect.StructField{
-			Name: name.Name,
-			Tag:  tag,
-		}
-
-		// NOTE: This function never returns an err, so
-		// we can safely ignore it.
-		mongoTags, _ := bsoncodec.DefaultStructTagParser(dummyStructField)
-		if mongoTags.Skip {
-			return nil
-		}
-
-		return &Field{
-			Name:    name.Name,
-			BsonKey: mongoTags.Name,
-			Type:    b.String(),
-		}
-	}
-
-	return nil
-}
-
-func parseReferencedTypes(
-	expr ast.Expr,
-	knownImports map[string]string,
-	usedImports map[string]struct{},
-	primitives map[string]struct{},
-) []string {
-	exprQueue := []ast.Expr{expr}
-	referencedTypes := []string{}
-
-	for len(exprQueue) != 0 {
-		e := exprQueue[0]
-		exprQueue = exprQueue[1:]
-
-		switch t := e.(type) {
-		case *ast.Ident:
-			// This is a local type or dot import
-			referencedTypes = append(referencedTypes, fmt.Sprintf(".%s", t.Name))
-		case *ast.SelectorExpr:
-			// This is a package import
-			pkgName, ok := t.X.(*ast.Ident)
-			if !ok {
-				panic(fmt.Sprintf("Struct package was not an identifier: %v", t.X))
-			}
-
-			typ := fmt.Sprintf("%s.%s", pkgName.Name, t.Sel.Name)
-			if _, ok := primitives[typ]; ok {
-				continue
-			}
-
-			imp, ok := knownImports[pkgName.Name]
-			if !ok {
-				panic(fmt.Sprintf(
-					"Import not found: %s (for type %s)",
-					t.Sel.Name, typ))
-			}
-
-			typeUID := fmt.Sprintf("%s.%s", imp, t.Sel.Name)
-			if _, ok := primitives[typ]; ok {
-				continue
-			}
-
-			referencedTypes = append(referencedTypes, typeUID)
-			usedImports[imp] = struct{}{}
-		case *ast.StarExpr:
-			exprQueue = append(exprQueue, t.X)
-			continue
-		case *ast.ArrayType:
-			exprQueue = append(exprQueue, t.Elt)
-			continue
-		case *ast.MapType:
-			exprQueue = append(exprQueue, t.Key)
-			exprQueue = append(exprQueue, t.Value)
-			break
-		case *ast.InterfaceType:
-			// Can't generate anything for interfaces, so we just skip them.
-			// TODO: Do we want to issue a warning for this?
-			continue
-		default:
-			panic(fmt.Sprintf("found unsupported field expression: %T\n", t))
-		}
-	}
-
-	return referencedTypes
-}
-
-func findTypeSpec(uid string) (*ast.TypeSpec, *token.FileSet, []*ast.ImportSpec, error) {
-	pkg, strct, err := splitPackageUID(uid)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	cfg := &packages.Config{Mode: packages.NeedFiles |
-		packages.NeedSyntax | packages.NeedTypes}
-
-	pkgs, err := packages.Load(cfg, pkg)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	for _, pkg := range pkgs {
-		for _, syn := range pkg.Syntax {
-			for _, decl := range syn.Decls {
-				d, ok := decl.(*ast.GenDecl)
-				if !ok {
-					continue
-				}
-
-				for _, spec := range d.Specs {
-					tspec, ok := spec.(*ast.TypeSpec)
-					if !ok {
-						continue
-					}
-
-					if strct == tspec.Name.String() {
-						return tspec, pkg.Fset, syn.Imports, nil
-					}
-				}
-			}
-		}
-	}
-
-	return nil, nil, nil, fmt.Errorf(
-		"Struct `%s` not found in module `%s`",
-		strct, pkg,
-	)
 }
 
 func splitPackageUID(uid string) (pkg, strct string, err error) {
